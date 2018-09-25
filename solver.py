b6db9bee1131afbf938c6b4d04ee12f6158e95f3
crossedxd <crossedxd@users.noreply.github.com>
2018-09-25 17:04:21 -0400

    separated counting logic out from fitness function

--- solver.py
+++ solver.py
@@ -1,9 +1,10 @@
 import math
 from collections import Counter
+import sudoku
 
 class Solver:
 
-    PENALTY_DUPLICATES = 10
+    PENALTY_DUPLICATES = 100
     PENALTY_BLANKS = 1
 
     def __init__(self):
@@ -12,24 +13,29 @@ class Solver:
     def get_fitness(self, puzzle):
         ''' Returns the fitness of a given puzzle. '''
         fitness = 0
+        fitness -= self.count_total_duplicates(puzzle)
+        fitness -= self.count_total_blanks(puzzle)
+        return fitness
 
+    def count_total_duplicates(self, puzzle):
+        ''' Returns a total count of duplicate values in a given puzzle. '''
         total_duplicates = 0
         for i in range(puzzle.SIZE):
             total_duplicates += self.count_duplicates(puzzle.get_row(i))
             total_duplicates += self.count_duplicates(puzzle.get_column(i))
-            total_duplicates += self.count_duplicates(puzzle.get_box(
+            total_duplicates += self.count_duplicates(puzzle.get_box_containing(
                 math.floor(i / puzzle.BOX_SIZE), i % puzzle.BOX_SIZE))
-        fitness -= total_duplicates * self.PENALTY_DUPLICATES
+        return total_duplicates * self.PENALTY_DUPLICATES
 
+    def count_total_blanks(self, puzzle):
+        ''' Returns a total count of blank values in a given puzzle. '''
         total_blanks = 0
         for i in range(puzzle.SIZE):
-            total_duplicates += self.count_blanks(puzzle.get_row(i))
-            total_duplicates += self.count_blanks(puzzle.get_column(i))
-            total_duplicates += self.count_blanks(puzzle.get_box(
+            total_blanks += self.count_blanks(puzzle.get_row(i))
+            total_blanks += self.count_blanks(puzzle.get_column(i))
+            total_blanks += self.count_blanks(puzzle.get_box_containing(
                 math.floor(i / puzzle.BOX_SIZE), i % puzzle.BOX_SIZE))
-        fitness -= total_blanks * self.PENALTY_BLANKS
-
-        return fitness
+        return total_blanks * self.PENALTY_BLANKS
 
     def count_duplicates(self, cells):
         ''' Returns a count of duplicate values in the given list of cells (excluding blanks). '''
