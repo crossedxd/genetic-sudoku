4b43346436f8ab99b5ec145191ce7c3941315c4c
crossedxd <crossedxd@users.noreply.github.com>
2018-09-26 00:05:02 -0400

    Added placeholders for GA

--- solver.py
+++ solver.py
@@ -4,10 +4,37 @@ import sudoku
 
 class Solver:
 
+    # Reference: https://towardsdatascience.com/introduction-to-genetic-algorithms-including-example-code-e396e98d8bf3
+    # More ref: https://www.tutorialspoint.com/genetic_algorithms/genetic_algorithms_quick_guide.htm
+
     PENALTY_DUPLICATES = 100
     PENALTY_BLANKS = 1
 
+    POPULATION_SIZE = 100
+    CROSSOVER_RATE = 1.0
+    MUTATION_RATE = 1.0
+    
+
     def __init__(self):
+        pass
+
+    def get_initial_generation(self):
+        pass
+
+    def select_next_generation(self, population):
+        '''
+        From the wiki: https://en.wikipedia.org/wiki/Selection_(genetic_algorithm)
+        1) Evaluate fitness of all members of the population, then normalize the fitness (divide by total fitness)
+        2) Sort descending on fitness values
+        3) Compute accumulated fitness values for each member
+        4) 
+        '''
+        pass
+
+    def perform_crossover(self, population):
+        pass
+
+    def perform_mutation(self, population):
         pass
 
     def get_fitness(self, puzzle):
