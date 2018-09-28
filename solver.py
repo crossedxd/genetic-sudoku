79ca2ef0d160bfb345a3b5e5a42aed61e5499193
crossedxd <sean.hofstadter@gmail.com>
2018-09-28 01:13:24 -0400

    Began implementing genetic algorithm using DEAP library

--- solver.py
+++ solver.py
@@ -1,74 +1,93 @@
 import math
+import random
 from collections import Counter
-import sudoku
+from deap import algorithms
+import numpy
+from deap import base
+from deap import creator
+from deap import tools
+from sudoku import Sudoku
 
-class Solver:
+PENALTY_DUPLICATES = 100
+PENALTY_BLANKS = 1
 
-    # Reference: https://towardsdatascience.com/introduction-to-genetic-algorithms-including-example-code-e396e98d8bf3
-    # More ref: https://www.tutorialspoint.com/genetic_algorithms/genetic_algorithms_quick_guide.htm
+POPULATION_SIZE = 1000
+MUTATION_RATE = 0.05
 
-    PENALTY_DUPLICATES = 100
-    PENALTY_BLANKS = 1
+FITNESS_MODEL = Sudoku()
 
-    POPULATION_SIZE = 100
-    CROSSOVER_RATE = 1.0
-    MUTATION_RATE = 1.0
-    
 
-    def __init__(self):
-        pass
+def evaluate(list_repr):
+    """ Returns the fitness of a given puzzle. """
+    FITNESS_MODEL.recreate_from_list(list_repr)
+    fitness = 0
+    fitness -= count_total_duplicates(FITNESS_MODEL)
+    fitness -= count_total_blanks(FITNESS_MODEL)
+    return fitness,
 
-    def get_initial_generation(self):
-        pass
 
-    def select_next_generation(self, population):
-        '''
-        From the wiki: https://en.wikipedia.org/wiki/Selection_(genetic_algorithm)
-        1) Evaluate fitness of all members of the population, then normalize the fitness (divide by total fitness)
-        2) Sort descending on fitness values
-        3) Compute accumulated fitness values for each member
-        4) 
-        '''
-        pass
+def count_total_duplicates(puzzle):
+    """ Returns a total count of duplicate values in a given puzzle. """
+    total_duplicates = 0
+    for i in range(puzzle.SIZE):
+        total_duplicates += count_duplicates(puzzle.get_row(i))
+        total_duplicates += count_duplicates(puzzle.get_column(i))
+        total_duplicates += count_duplicates(puzzle.get_box_containing(
+            math.floor(i / puzzle.BOX_SIZE), i % puzzle.BOX_SIZE))
+    return total_duplicates * PENALTY_DUPLICATES
 
-    def perform_crossover(self, population):
-        pass
 
-    def perform_mutation(self, population):
-        pass
+def count_total_blanks(puzzle):
+    """ Returns a total count of blank values in a given puzzle. """
+    total_blanks = 0
+    for i in range(puzzle.SIZE):
+        total_blanks += count_blanks(puzzle.get_row(i))
+        total_blanks += count_blanks(puzzle.get_column(i))
+        total_blanks += count_blanks(puzzle.get_box_containing(
+            math.floor(i / puzzle.BOX_SIZE), i % puzzle.BOX_SIZE))
+    return total_blanks * PENALTY_BLANKS
 
-    def get_fitness(self, puzzle):
-        ''' Returns the fitness of a given puzzle. '''
-        fitness = 0
-        fitness -= self.count_total_duplicates(puzzle)
-        fitness -= self.count_total_blanks(puzzle)
-        return fitness
 
-    def count_total_duplicates(self, puzzle):
-        ''' Returns a total count of duplicate values in a given puzzle. '''
-        total_duplicates = 0
-        for i in range(puzzle.SIZE):
-            total_duplicates += self.count_duplicates(puzzle.get_row(i))
-            total_duplicates += self.count_duplicates(puzzle.get_column(i))
-            total_duplicates += self.count_duplicates(puzzle.get_box_containing(
-                math.floor(i / puzzle.BOX_SIZE), i % puzzle.BOX_SIZE))
-        return total_duplicates * self.PENALTY_DUPLICATES
+def count_duplicates(cells):
+    """ Returns a count of duplicate values in the given list of cells (excluding blanks). """
+    counts = Counter(cells)
+    return sum([counts[i] for i in counts if counts[i] > 1 and i != 0])
 
-    def count_total_blanks(self, puzzle):
-        ''' Returns a total count of blank values in a given puzzle. '''
-        total_blanks = 0
-        for i in range(puzzle.SIZE):
-            total_blanks += self.count_blanks(puzzle.get_row(i))
-            total_blanks += self.count_blanks(puzzle.get_column(i))
-            total_blanks += self.count_blanks(puzzle.get_box_containing(
-                math.floor(i / puzzle.BOX_SIZE), i % puzzle.BOX_SIZE))
-        return total_blanks * self.PENALTY_BLANKS
 
-    def count_duplicates(self, cells):
-        ''' Returns a count of duplicate values in the given list of cells (excluding blanks). '''
-        counts = Counter(cells)
-        return sum([counts[i] for i in counts if counts[i] > 1 and i != 0])
+def count_blanks(cells):
+    """ Returns a count of blank values in the given list of cells. """
+    return sum([1 for i in cells if i == 0])
 
-    def count_blanks(self, cells):
-        ''' Returns a count of blank values in the given list of cells. '''
-        return sum([1 for i in cells if i == 0])
+
+def main():
+    creator.create('FitnessMax', base.Fitness, weights=(1.0,))
+    creator.create('Individual', list, fitness=creator.FitnessMax)
+
+    toolbox = base.Toolbox()
+    toolbox.register('attr_val', random.randint, 0, 9)
+
+    toolbox.register('individual', tools.initRepeat, creator.Individual, toolbox.attr_val, 81)
+    toolbox.register('population', tools.initRepeat, list, toolbox.individual)
+
+    toolbox.register('evaluate', evaluate)
+    toolbox.register('mate', tools.cxTwoPoint)
+    toolbox.register('mutate', tools.mutUniformInt, low=1, up=9, indpb=MUTATION_RATE)
+    toolbox.register('select', tools.selTournament, tournsize=3)
+
+    pop = toolbox.population(n=POPULATION_SIZE)
+    hof = tools.HallOfFame(1)
+    stats = tools.Statistics(lambda ind: ind.fitness.values)
+    stats.register('avg', numpy.mean)
+    stats.register('std', numpy.std)
+    stats.register('min', numpy.min)
+    stats.register('max', numpy.max)
+
+    pop, log = algorithms.eaSimple(pop, toolbox, cxpb=0.5, mutpb=0.2, ngen=100, stats=stats,
+                                   halloffame=hof, verbose=True)
+
+    print(hof)
+    return pop, log, hof
+
+
+if __name__ == '__main__':
+    main()
